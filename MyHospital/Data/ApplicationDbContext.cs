469ece6da6a7e22f0ed69042270c109934fd5ae4
MMFPR <eng.mmf.26@gmail.com>
2025-09-12 20:32:10 +0300

    Add project files.

---
+++ MyHospital/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyHospital.Models;
+
+namespace MyHospital.Data
+{
+
+
+
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; } 
+
+    }
+
+}
---
+++ MyHospital/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace MyHospital.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }              // المفتاح الأساسي (Primary Key)
+        public string Name { get; set; }         // اسم التصنيف
+        public string Description { get; set; }  // وصف التصنيف (اختياري)
+    }
+}
