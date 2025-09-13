fa58e20751fc0454236e1b8a58125f8c1cf5ad38
MMFPR <eng.mmf.26@gmail.com>
2025-09-13 14:30:03 +0300

    Refactored category handling: replaced static HTML entries with dynamic database-driven rendering to support any number of medical categories

--- MyHospital/Controllers/CategoryController.cs
+++ MyHospital/Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyHospital.Data;
+using MyHospital.Models;
 
 namespace MyHospital.Controllers
 {
     public class CategoryController : Controller
     { 
+
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> categories = _context.Categories.ToList();
+            return View(categories);
         }
     }
 }
--- MyHospital/Models/Category.cs
+++ MyHospital/Models/Category.cs
@@ -1,7 +1,10 @@
-﻿namespace MyHospital.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyHospital.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }              // المفتاح الأساسي (Primary Key)
         public string Name { get; set; }         // اسم التصنيف
         public string Description { get; set; }  // وصف التصنيف (اختياري)
