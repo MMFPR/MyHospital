9edbdd25de15a4721b3c6e79816a860b08abcff8
MMFPR <eng.mmf.26@gmail.com>
2025-09-19 01:33:55 +0300

    Control the addition, modification and deletion of medical categories through the dashboard

--- MyHospital/Models/Category.cs
+++ MyHospital/Models/Category.cs
@@ -5,8 +5,9 @@ namespace MyHospital.Models
     public class Category
     {
         [Key]
-        public int Id { get; set; }              // المفتاح الأساسي (Primary Key)
-        public string Name { get; set; }         // اسم التصنيف
-        public string Description { get; set; }  // وصف التصنيف (اختياري)
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
     }
 }
+ 
